f5523d97af1df9afad9659765f641ea443756899
Leon Michalak <leonmichalak6@gmail.com>
2022-04-12 13:58:39 +0100

    Hero image slider next buttons added, javascript working

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Sidebar
+
 const navbarBtn = document.getElementById('navbar-btn');
 const navbarOpenIcon = document.getElementById('navbar-open');
 const navbarCloseIcon = document.getElementById('navbar-close');
@@ -10,3 +12,20 @@ navbarBtn.addEventListener('click', () => {
 
   sidebar.classList.toggle('sidebar--active');
 });
+
+// Hero section
+const headerContainer = document.getElementsByClassName('header__container')[0];
+const headerBtns = document.getElementsByClassName('header__next-btn');
+
+for (let i = 0; i < headerBtns.length; i++) {
+  headerBtns[i].addEventListener('click', () => {
+    // Disable active class on other buttons and add to current
+    for (let j = 0; j < headerBtns.length; j++) {
+      headerBtns[j].classList.remove('header__next-btn--active');
+    }
+
+    headerBtns[i].classList.add('header__next-btn--active');
+
+    headerContainer.id = `header__container-${headerBtns[i].innerHTML}`;
+  });
+}
